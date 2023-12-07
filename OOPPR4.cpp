bd158f93d1a01c68900a21f62a9366662dde4585
Your Name <pirjademl7@gmail.com>
2023-12-07 14:03:06 +0530

    update OOP Practical 4

--- OOPPR4.cpp
+++ OOPPR4.cpp
@@ -1,36 +1,113 @@
+
+// Practical 4  FIle Handling oop
 #include <iostream>
+#include <stdlib.h>
 #include <fstream>
 using namespace std;
+class Student
+{
+public:
+    char name[20];
+    string phone;
+
+public:
+    void accept();
+    void display();
+};
+void Student::accept()
+{
+    cin >> name;
+    cin >> phone;
+}
+void Student::display()
+{
+    cout << name << " " << phone << endl;
+}
 int main()
 {
-    fstream my_file;
-    my_file.open("my_file.txt", ios::out);
-    if (!my_file)
+    int ch;
+    int limit;
+    cout << "Enter the number of people whose information is to be stored" << endl;
+    cin >> limit;
+    Student *a = new Student[limit];
+    fstream file;
+    file.open("ry_4.txt", ios::in | ios::out);
+    do
     {
-        cout << "File not created!";
-    }
-    else
-    {
-        cout << "File created successfully!";
-        my_file << "sahilm";
-        my_file.close();
-    }
-    my_file.open("my_file.txt", ios::in);
-    if (!my_file)
-    {
-        cout << "No such file";
-    }
-    else
-    {
-        char ch;
-        while (1)
+        cout << "1.Store 2.Display 3.Search 4.Modify 5.Exit" << endl;
+        cin >> ch;
+        switch (ch)
         {
-            my_file >> ch;
-            if (my_file.eof())
+        case 1:
+            cout << "Enter the name and phone number" << endl;
+            for (int i = 0; i < limit; i++)
+            {
+                a[i].accept();
+
+                file.write((char *)&a[i], sizeof(a[i]));
+            }
+            break;
+        case 2:
+            cout << "Stored information are" << endl;
+            for (int i = 0; i < limit; i++)
+            {
+                a[i].display();
+                file.read((char *)&a[i], sizeof(a[i]));
+            }
+            break;
+        case 3:
+            int ch1;
+            cout << "1.By name 2.By phone" << endl;
+            cin >> ch1;
+            switch (ch1)
+            {
+            case 1:
+                char name1[20];
+                cout << "Enter the name" << endl;
+                cin >> name1;
+                for (int i = 0; i < limit; i++)
+                {
+                    if (a[i].name == name1)
+                    {
+                        file.read((char *)&a[i], sizeof(a[i]));
+                        cout << a[i].phone << endl;
+                    }
+                }
                 break;
-            cout << ch;
+            case 2:
+                string phone1;
+                cout << "Enter the phone number of the person" << endl;
+                cin >> phone1;
+                for (int i = 0; i < limit; i++)
+                {
+                    if (a[i].phone == phone1)
+                    {
+
+                        file.read((char *)&a[i], sizeof(a[i]));
+                        cout << a[i].name << endl;
+                    }
+                }
+                break;
+            }
+            break;
+        case 4:
+            string phone2, phone3;
+
+            cout << "Enter the old telephone number which u want to modify" << endl;
+            cin >> phone2;
+            for (int i = 0; i < limit; i++)
+            {
+                if (a[i].phone == phone2)
+                {
+                    file.read((char *)&a[i], sizeof(a[i]));
+                    cout << "Enter new phone number" << endl;
+                    cin >> phone3;
+                    a[i].phone == phone3;
+                    file.write((char *)&a[i], sizeof(a[i]));
+                }
+            }
+            break;
         }
-    }
-    my_file.close();
+    } while (ch != 5);
     return 0;
 }
